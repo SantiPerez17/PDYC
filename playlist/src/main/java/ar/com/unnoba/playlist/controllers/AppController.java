847b49be9a43c8519741845f6790a0e2cea347ca
SantiPerez17 <santi__@live.com.ar>
2021-04-27 12:18:33 -0300

    commit 1
    Se agrega el proyecto de SB

---
+++ playlist/src/main/java/ar/com/unnoba/playlist/controllers/AppController.java
@@ -0,0 +1,16 @@
+package ar.com.unnoba.playlist.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	/**
+	 *
+	 */
+	@GetMapping({"/","/home"})
+	public String index() {
+		return "home";
+	}
+}
